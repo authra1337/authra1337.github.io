7aa2b5539b781058f89dd0affa2b2972ed030bb7
authra <47796353+authra1337@users.noreply.github.com>
2022-08-24 17:43:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+})
